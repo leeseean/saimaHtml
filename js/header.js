c402d32f90564f9d5bf394da1daf6095421c191e
leeseean chiu <leeseean@qq.com>
2017-06-23 08:32:36 +0800

    嘻嘻嘻

---
+++ js/header.js
@@ -0,0 +1,7 @@
+// $(function(){
+//     $('nav>ul.ulNav>li').hover(function(){
+//         $(this).next('ul').css({'display':'block'});
+//     },function(){
+//         $(this).next('ul').css({'display':'none'});
+//     });
+// });
