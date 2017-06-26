1e6e1e5f6232cd30deedf3c4c17df459dced7520
leeseean <leeseean@qq.com>
2017-06-26 11:08:23 +0800

    add

--- js/common.js
+++ js/common.js
@@ -3,17 +3,17 @@ function changeFontSizeRe(num){
         case 0:
             $('#fontSizeContainer>div>a').removeClass('active');
             $('#fontSizeContainer>div>a.small').addClass('active');
-            $('.member-content-container,.corporate-content-container,.charities-content-container').css('font-size','90%');
+            $('.member-content-container,.corporate-content-container,.charities-content-container,.home-content-container').css('font-size','90%');
             break;
         case 1:
             $('#fontSizeContainer>div>a').removeClass('active');
             $('#fontSizeContainer>div>a.medium').addClass('active');
-            $('.member-content-container,.corporate-content-container,.charities-content-container').css('font-size','100%');
+            $('.member-content-container,.corporate-content-container,.charities-content-container,.home-content-container').css('font-size','100%');
             break;
         case 2:
             $('#fontSizeContainer>div>a').removeClass('active');
             $('#fontSizeContainer>div>a.large').addClass('active');
-            $('.member-content-container,.corporate-content-container,.charities-content-container').css('font-size','120%');
+            $('.member-content-container,.corporate-content-container,.charities-content-container,.home-content-container').css('font-size','120%');
             break;
     }
 }
